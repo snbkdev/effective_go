f0c8c5d10c945397a6f128dafe86fed2491cfd41
Asanbek09 <samudinua@gmail.com>
2026-02-04 09:54:00 +0600

    chapter 02 - execution tracing - примеры

---
+++ chapter_02/execution_tracing/add_tracing/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+	"runtime/trace"
+)
+
+func main() {
+	file, err := os.Create("trace.out")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	err = trace.Start(file)
+	if err != nil {
+		panic(err)
+	}
+	defer trace.Stop()
+}
